238b4994528848fe9103685037f4ead0084d161d
/code/$GITHUB_USERNAME/dotfiles <diego.delaminne@gmail.com>
2018-10-29 14:06:04 +0100

    quetions controler

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @message = params[:question]
+    case answer
+    when message == "I am going to work"
+      @answer = "Great!"
+    when message.include?("?") == true
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
